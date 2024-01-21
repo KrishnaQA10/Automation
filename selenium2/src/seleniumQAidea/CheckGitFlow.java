690f73beca0eadc521cd8021552cc31cff1f2941
[Krishna Sharma] <[Sharmaakri10@gmail.com]>
2024-01-21 18:55:08 +0530

    Check the git flow

---
+++ selenium2/src/seleniumQAidea/CheckGitFlow.java
@@ -0,0 +1,21 @@
+package seleniumQAidea;
+
+import org.testng.annotations.Test;
+
+public class CheckGitFlow {
+	
+	@Test
+	public void checkGitflow(){ 
+		
+		System.out.println("HEllo Krishna");
+		System.out.println("HEllo Krishna");	
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
