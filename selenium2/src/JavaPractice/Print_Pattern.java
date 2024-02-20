dabec4491560441a7bc0d648bbb69ad0abbe6b95
[Krishna Sharma] <[Sharmaakri10@gmail.com]>
2024-02-20 15:37:50 +0530

    java practice

---
+++ selenium2/src/JavaPractice/Print_Pattern.java
@@ -0,0 +1,18 @@
+package JavaPractice;
+
+
+//Write a pattern in java like 1 2 3 4 5 6 7 8
+public class Print_Pattern {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i<=10; i++)
+		{
+		System.out.print(i + " ");
+		}
+		
+}
+	
+	
+
+}
